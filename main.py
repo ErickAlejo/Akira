4a3385083472a63e199dcfe9e8763c6a84b4a585
unknown <erickgraterol@gmail.com>
2023-05-11 22:10:56 -0500

    Remove Emojis and Chars specials

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import time
 import re
 import getpass
 
-host = input("🚧 Host: ")
-username = input("🚧 Username: ")
-password = getpass.getpass("   Password: ")
+host = input(" Host: ")
+username = input(" Username: ")
+password = getpass.getpass(" Password: ")
 
 conf = {
 	"host": host,
@@ -21,6 +21,26 @@ def create_json(data, filename):
 	with open(filename, 'w') as file:
 		json.dump(data, file,indent=4)
 
+# Function special to make a speed_test a capture screenshots
+def speed_test(host,username,password,address):
+		# Configuración del router MikroTik
+		host = host
+		username = username
+		password = password
+
+		# Conexión al router MikroTik
+		api = connect(host=host, username=username, password=password)
+
+		# Ejecución del speed-test
+		result = api(cmd='/tool/speed-test', address=address, user=username, password=password, duration="30")
+		speed = []
+		data = {
+			"speed": speed,
+			"wan": address
+		}
+		for a in result:
+		    speed.append(a)
+		return speed
 
 def exec_command(cmd): # Launch a command
 	value = []
@@ -42,7 +62,7 @@ def exec_command(cmd): # Launch a command
 
 def pretty_print_hostname(data):
 	if data['hostnames']:
-		print(f"🏠  {data['hostnames'][0]['name']}\n")
+		print(f" {data['hostnames'][0]['name']}\n")
 	else:
 		print(f"❌ Not Found Hostname")
 
@@ -56,36 +76,30 @@ def pretty_print_ipaddress(data):
 		ips = a['address'].split('/')
 		ips = ips[0]
 		if re.match(ips_links,ips):
-			print(f"✅ IPs Radio: {ips} - {a['interface']}")
+			print(f" Links: {ips} ({a['interface']})")
 		elif re.match(ips_customer,ips):
-			print(f"✅ IPs Customer: {ips} - {a['interface']}")
+			print(f" Customers: {ips} ({a['interface']})")
 		elif re.match(ips_wan,ips):
-			print(f"✅ IPs WAN: {ips} - {a['interface']}")
+			print(f" WANs: {ips} ({a['interface']})")
 		elif re.match(ips_lo, ips):
-			print(f"✅ IPs Lo0: {ips} -	{a['interface']}")
+			print(f" Lo0: {ips} ({a['interface']})")
 
 def pretty_print_interface(data):
 	inter = data['interface']
 	if inter:
 		for a in inter:
 			if a['speed'] == '1Gbps' or a['speed'] == '10Gbps':
-				print(f"✅ Interface {a['name']} mtu {a['mtu']} {a['speed']} ")
+				print(f" Interface {a['name']} mtu {a['mtu']} {a['speed']} ")
 			else:
-				print(f"❌ Interface {a['name']} {a['speed']}")
+				print(f" Interface {a['name']} {a['speed']}")
 	else:
-		print('❌ Not Found Interface')
+		print(' Not Found Interface')
 
 def pretty_print_neighbor(data):
 	neighbors = []
 	for a in data['neighbors']:
 		identity = a['identity']
-		identity = identity.split('_')
-		fin_identity = "_".join(identity)
-
-		if 'CN' in identity:
-			print(f'✅ Neighbor: {fin_identity}')
-		else:
-			print(f'⚠️ Others: {fin_identity}')
+		print(f"Neighbor: {identity}")
 
 def pretty_print_ospf_neighbor(data):
 	wan = ''
@@ -95,9 +109,9 @@ def pretty_print_ospf_neighbor(data):
 		for a in data['ospf-neighbor']:
 			count += 1
 			wan = a['address']
-			print(f"✅ Neighbor: {a['address']} - {a['state']} - {a['adjacency']} - {a['interface']}")
+			print(f" Neighbor: {a['address']} - {a['state']} - {a['adjacency']} - {a['interface']}")
 	else:
-		print('❌ Not Found OSPF Neighbor')
+		print(' Not Found OSPF Neighbor')
 
 	return wan
 
@@ -108,30 +122,29 @@ def pretty_print_ospf_lsas(data):
 		if a['id'] == '0.0.0.0':
 			default.append({'id':a['id'],'type':a['type'],'originator':a['originator']})
 	if len(default) == 2:
-		print(f'✅ Default: {default[0]["id"]} - Originator: {default[0]["originator"]}')
-		print(f'✅ Default: {default[1]["id"]} - Originator: {default[1]["originator"]}')
+		print(f' Default: {default[0]["id"]} - Originator: {default[0]["originator"]}')
+		print(f' Default: {default[1]["id"]} - Originator: {default[1]["originator"]}')
 	elif len(default) > 2 :
-		print('⚠️ LSAs (> 2) ')
-		print(f'❌ Default: {default[0]["id"]} - Originator: {default[0]["originator"]}')
-		print(f'❌ Default: {default[1]["id"]} - Originator: {default[1]["originator"]}')
-		print(f'❌ Default: {default[2]["id"]} - Originator: {default[2]["originator"]}')
+		print(' LSAs (> 2) ')
+		print(f' Default: {default[0]["id"]} - Originator: {default[0]["originator"]}')
+		print(f' Default: {default[1]["id"]} - Originator: {default[1]["originator"]}')
+		print(f' Default: {default[2]["id"]} - Originator: {default[2]["originator"]}')
 	elif len(default)  == 1:
-		print(f'❌ Default: {default[0]["id"]} - Originator: {default[0]["originator"]}')
+		print(f' Default: {default[0]["id"]} - Originator: {default[0]["originator"]}')
 	else:
-		print('❌ Not Found LSAs')
+		print(' Not Found LSAs')
 
 
 	# Radius
-	print('\n--------------------- Radius ----------------------- \n')
+	print('\n--- Radius --- \n')
 	if len(data['radius']) > 0:	
 		for a in data['radius']:
-			print('✅')
-			print(f"comment: {a['comment']} ")
-			print(f"address: {a['address']}")
-			print(f"service: {a['service']} ")
-			print(f"proto: {a['protocol']}")
+			print(f" comment: {a['comment']} ")
+			print(f" address: {a['address']}")
+			print(f" service: {a['service']} ")
+			print(f" proto: {a['protocol']}")
 	else:
-		print('❌ Not exist radius')
+		print(' Not exist radius')
 
 
 def pretty_print_speed_test(data,wan):
@@ -151,127 +164,119 @@ def pretty_print_speed_test(data,wan):
 			upload = tcpupload.split('Mbps')[0]
 			
 			if int(download) < 500 and int(upload) < 500:	# If Download < 500M 
-				print(f"✅ Address: 100.127.0.3")
-				print(f"❌ TCP Download: {tcpdownload}")
-				print(f"❌ TCP Upload: {tcpupload}")
-				print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-				print(f"✅ Ping: {speed['ping-min-avg-max']}")
+				print(f" Address: 100.127.0.3")
+				print(f" TCP Download: {tcpdownload}")
+				print(f" TCP Upload: {tcpupload}")
+				print(f" Jitter {speed['jitter-min-avg-max']}")
+				print(f" Ping: {speed['ping-min-avg-max']}")
 			else: 					# Else everything ok ...
-				print(f"✅ Address: 100.127.0.3")
-				print(f"✅ TCP Download: {tcpdownload}")
-				print(f"✅ TCP Upload: {tcpupload}")
-				print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-				print(f"✅ Ping: {speed['ping-min-avg-max']}")
+				print(f" Address: 100.127.0.3")
+				print(f" TCP Download: {tcpdownload}")
+				print(f" TCP Upload: {tcpupload}")
+				print(f" Jitter {speed['jitter-min-avg-max']}")
+				print(f" Ping: {speed['ping-min-avg-max']}")
 		elif re.match(regex_Mbps, tcpupload):
 			download = tcpdownload.split('Mbps')[0]
 			upload = tcpupload.split('Mbps')[0]
 
 			if int(download) < 500 and int(upload) < 500:	# If Download < 500M 
-				print(f"✅ Address: 100.127.0.3")
-				print(f"❌ TCP Download: {tcpdownload}")
-				print(f"❌ TCP Upload: {tcpupload}")
-				print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-				print(f"✅ Ping: {speed['ping-min-avg-max']}")
+				print(f" Address: 100.127.0.3")
+				print(f" TCP Download: {tcpdownload}")
+				print(f" TCP Upload: {tcpupload}")
+				print(f" Jitter {speed['jitter-min-avg-max']}")
+				print(f" Ping: {speed['ping-min-avg-max']}")
 			else: 					# Else everything ok ...
-				print(f"✅ Address: 100.127.0.3")
-				print(f"✅ TCP Download: {tcpdownload}")
-				print(f"✅ TCP Upload: {tcpupload}")
-				print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-				print(f"✅ Ping: {speed['ping-min-avg-max']}")
+				print(f" Address: 100.127.0.3")
+				print(f" TCP Download: {tcpdownload}")
+				print(f" TCP Upload: {tcpupload}")
+				print(f" Jitter {speed['jitter-min-avg-max']}")
+				print(f" Ping: {speed['ping-min-avg-max']}")
 		elif re.match(regex_bps, tcpdownload):
 			download = tcpdownload.split('Mbps')[0]
 			upload = tcpupload.split('Mbps')[0]
 			
-			print(f"✅ Address: 100.127.0.3")
-			print(f"❌ TCP Download: {tcpdownload}")
-			print(f"❌ TCP Upload: {tcpupload}")
-			print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-			print(f"✅ Ping: {speed['ping-min-avg-max']}")
+			print(f" Address: 100.127.0.3")
+			print(f" TCP Download: {tcpdownload}")
+			print(f" TCP Upload: {tcpupload}")
+			print(f" Jitter {speed['jitter-min-avg-max']}")
+			print(f" Ping: {speed['ping-min-avg-max']}")
 		
 		elif re.match(regex_bps, tcpupload):
 			download = tcpdownload.split('Mbps')[0]
 			upload = tcpupload.split('Mbps')[0]
 
-			print(f"✅ Address: 100.127.0.3")
-			print(f"❌ TCP Download: {tcpdownload}")
-			print(f"❌ TCP Upload: {tcpupload}")
-			print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-			print(f"✅ Ping: {speed['ping-min-avg-max']}")
+			print(f" Address: 100.127.0.3")
+			print(f" TCP Download: {tcpdownload}")
+			print(f" TCP Upload: {tcpupload}")
+			print(f" Jitter {speed['jitter-min-avg-max']}")
+			print(f" Ping: {speed['ping-min-avg-max']}")
 
 		elif re.match(regex_Gbps, tcpdownload):
-			print(f"✅ Address: 100.127.0.3")
-			print(f"✅ TCP Download: {tcpdownload}")
-			print(f"✅ TCP Upload: {tcpupload}")
-			print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-			print(f"✅ Ping: {speed['ping-min-avg-max']}")
+			print(f" Address: 100.127.0.3")
+			print(f" TCP Download: {tcpdownload}")
+			print(f" TCP Upload: {tcpupload}")
+			print(f" Jitter {speed['jitter-min-avg-max']}")
+			print(f" Ping: {speed['ping-min-avg-max']}")
 		
 		elif re.match(regex_Gbps, tcpupload):
-			print(f"✅ Address: 100.127.0.3")
-			print(f"✅ TCP Download: {tcpdownload}")
-			print(f"✅ TCP Upload: {tcpupload}")
-			print(f"✅ Jitter {speed['jitter-min-avg-max']}")
-			print(f"✅ Ping: {speed['ping-min-avg-max']}")
+			print(f" Address: 100.127.0.3")
+			print(f" TCP Download: {tcpdownload}")
+			print(f" TCP Upload: {tcpupload}")
+			print(f" Jitter {speed['jitter-min-avg-max']}")
+			print(f" Ping: {speed['ping-min-avg-max']}")
 	else:
-		print("❌ Not Work Speed-test")
+		print("Not Work Speed-test")
 
 
-def speed_test(host,username,password,address):
-		# Configuración del router MikroTik
-		host = host
-		username = username
-		password = password
 
-		# Conexión al router MikroTik
-		api = connect(host=host, username=username, password=password)
+def pretty_print_routerboard(data):
+	if data['routerboard']:
+		for a in data['routerboard']:
+			print(f"Model: {a['model']}")
+			print(f"Factory: {a['factory-firmware']}")
+			print(f"Current: {a['current-firmware']}")
+			print(f"Upgrade: {a['upgrade-firmware']}")
+	else:
+		print('Not Found Routerboard')
 
-		# Ejecución del speed-test
-		result = api(cmd='/tool/speed-test', address=address, user=username, password=password, duration="30")
-		speed = []
-		data = {
-			"speed": speed,
-			"wan": address
-		}
-		for a in result:
-		    speed.append(a)
-		return speed
 
-#--------------------------------------------------------------#
-# OUTPUT generate by function analyze_json()                   #
-# If you want modify output, please check the function analyze #
-#--------------------------------------------------------------#
+
 
 def analyze_json_atp(data): # Generate Analyze ATP
 	# Hostname
-	print('\n--------------- Hostname --------------------- \n')
+	print('\n--- Hostname --- \n')
 	pretty_print_hostname(data)
 
 	# IP address
-	print('\n------------------- IP --------------------- \n')
+	print('\n--- IP --- \n')
 	pretty_print_ipaddress(data)
     
 
     # Interfaces
-	print('\n------------------ Interface ---------------------- \n')
+	print('\n--- Interface --- \n')
 	pretty_print_interface(data)
 
 	# IP Neighbors
-	print('\n------------------- Neighbor ----------------------- \n')
+	print('\n--- Neighbor --- \n')
 	pretty_print_neighbor(data)
 
 	# OSPF Neighbor
-	print('\n----------------------- OSPF ----------------------- \n')
+	print('\n--- OSPF Neighbor ---- \n')
 	pretty_print_ospf_neighbor(data)
 	wan = pretty_print_ospf_neighbor(data)
 
 	# OSPF LSAs
-	print('\n----------------------- OSPF ----------------------- \n')
+	print('\n--- OSPF LSAs --- \n')
 	pretty_print_ospf_lsas(data)
 	
 	#Speed-test
-	print('\n------------------- Speed-test WAN ------------------\n')
-	pretty_print_speed_test(data,wan)
+	#print('\n--- Speed-test ---\n')
+	#pretty_print_speed_test(data,wan)
 
-	print('\n----- Script Generated by Eirikr -----\n')
+	print('\n--- Firmware & Router ---\n')
+	pretty_print_routerboard(data)
+
+	print('\n--- Script By Team Red Rollout ---\n')
 
 	
 
@@ -282,7 +287,7 @@ try:
 	password=conf['password']
 	)
 
-	print('☕ Please wait a moment... \n')
+	print(' Please wait a moment... \n')
 	# Launch Commands
 	hostnames = exec_command(cmd='/system/identity/print')
 	interface = exec_command(cmd='/interface/ethernet/print')
@@ -291,7 +296,8 @@ try:
 	radius  = exec_command(cmd='/radius/print')
 	ospf_lsa  = exec_command(cmd='/routing/ospf/lsa/print')
 	ospf_neighbor = exec_command(cmd='/routing/ospf/neighbor/print')
-	speed_test_core = speed_test(conf['host'],conf['username'],conf['password'],'100.127.0.3')
+	routerboard = exec_command(cmd='/system/routerboard/print')
+	#speed_test_core = speed_test(conf['host'],conf['username'],conf['password'],'100.127.0.3')
 
 	# Filters 
 	hostname  = [{'name':el['name']} for el in hostnames]
@@ -301,8 +307,9 @@ try:
 	radius = [{'comment':el['comment'],'address':el['address'],'src-address':el['src-address'],'protocol':el['protocol'],'service':el['service']} for el in radius]
 	ospf_lsa = [{'area':el['area'],'id':el['id'],'type':el['type'], 'originator':el['originator']} for el in ospf_lsa]
 	ospf_neighbor = [{'address':el['address'],'state':el['state'],'adjacency':el['adjacency'], 'interface':el['interface']} for el in ospf_neighbor]
+	routerboard = [{'model':el['model'],'factory-firmware':el['factory-firmware'],'current-firmware':el['current-firmware'],'upgrade-firmware':el['upgrade-firmware']} for el in routerboard]
 
-	# Generate Object with Filters
+	# Generate Object Full
 	data = {}
 
 	data['hostnames'] = hostnames 
@@ -312,14 +319,15 @@ try:
 	data['radius'] = radius
 	data['ospf-neighbor'] = ospf_neighbor
 	data['ospf'] = ospf_lsa
-	data['speed-test-core'] = speed_test_core
+	#data['speed-test-core'] = speed_test_core
+	data['routerboard'] = routerboard
 
 	# Generate ATP Analyze
 	analyze_json_atp(data)
 
 except ConnectionRefusedError:
-	print('Please allow API Port ☕')
+	print('Please Allow API Port')
 except KeyboardInterrupt:
-	print('Bye ...')
+	print('Bye')
 except librouteros.exceptions.TrapError:
-	print('Bad Username or Password')
+	print('Error User or Password')
